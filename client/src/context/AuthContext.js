154f06dc4c47ff05300081083c5681a98f05cf9b
Safak <safak.kocaoglu@bil.omu.edu.tr>
2021-04-07 10:14:55 +0200

    storage

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,26 +1,22 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-  user: {
-    _id: "605b3dc77a3cf74918165c06",
-    username: "jane",
-    email: "jane@gmail.com",
-    profilePicture: "person/1.jpeg",
-    coverPicture: "",
-    isAdmin: false,
-    followers: [],
-    followings: [],
-  },
+  user:JSON.parse(localStorage.getItem("user")) || null,
   isFetching: false,
   error: false,
 };
+
 
 export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
-
+  
+  useEffect(()=>{
+    localStorage.setItem("user", JSON.stringify(state.user))
+  },[state.user])
+  
   return (
     <AuthContext.Provider
       value={{
