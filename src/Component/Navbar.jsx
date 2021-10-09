cf26537f106755744638a32b779df1105b8d869d
Addis Belete <addis.bel@gmail.com>
2021-10-09 12:05:19 +0300

    Add NavBar component

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+	return (
+		<div>
+		Navbar	
+		</div>
+	)
+}
+
+export default Navbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Navbar from '../Component/Navbar'
 const Home = () => {
 	return (
 		<div>
-		Home page	
+		<Navbar/>	
 		</div>
 	)
 }
