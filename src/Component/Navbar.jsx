4f243e40a33d76222bea8bbadd97fd50e3efef6e
Addis Belete <addis.bel@gmail.com>
2021-10-09 12:32:26 +0300

    Install material Ui

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+height: 60px;
 
+`
+const Wrapper = styled.div`
+padding: 10px 20px;
+display: flex;
+justify-content: space-between;
+`
+const Left = styled.div`
+flex: 1;
+`
+const Language = styled.span`
+font-size:12px;
+cursor: pointer;
+`
+const Center = styled.div`
+flex: 1;
+`
+const Right = styled.div`
+flex: 1;
+`
 const Navbar = () => {
 	return (
-		<div>
-		Navbar	
-		</div>
+		<Container>
+<Wrapper>
+<Left>
+<Language>Eng</Language>
+
+</Left>	
+<Center>Center</Center>	
+<Right>Right</Right>
+	
+</Wrapper>
+		</Container>
 	)
 }
 
