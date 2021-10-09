dc961549cb3f3b59ed6d25bebce42e9701ee016f
Addis Belete <addis.bel@gmail.com>
2021-10-09 13:30:59 +0300

    Add announcement component

---
+++ src/Component/Announcement.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+height: 30px;
+background-color: teal;
+color:white;
+`
+const Announcement = () => {
+	return (
+		<Container>
+Super Deal! Free Shipping on Order Over $50
+
+</Container>
+	)
+}
+
+export default Announcement
