51a614b577c32d7e47580e53647dd8ffd951982f
Addis Belete <addis.bel@gmail.com>
2021-10-11 11:21:22 +0300

    Add a props for arrows

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -5,21 +5,34 @@ const Container = styled.div`
 width: 100%;
 height:100vh;
 display: flex;
+background-color: coral;
+position: relative;
 `
 const Arrow = styled.div`
 width: 50px;
 height:50px;
 background: white;
 border-radius: 50%;
+display: flex;
+align-items: center;
+justify-content: center;
+position:absolute;
+top:0;
+bottom: 0;
+left:${props=>props.direction === "left" && "10px" };
+right:${props=> props.direction === "right" && "10px"};
+margin: auto;
+cursor: pointer;
+opacity:0.5;
 `
 const Slider = () => {
 	return (
 		<Container>
-			<Arrow>
+			<Arrow direction = "left">
 <ArrowLeftOutlined/>
 </Arrow>
-<Arrow>
-<ArrowRightOutlined>
+<Arrow direction = "right">
+<ArrowRightOutlined/>
 </Arrow>
 		</Container>
 	)
