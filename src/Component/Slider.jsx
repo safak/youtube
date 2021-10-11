75c9d5bd4a85acd625cc0ae7bdd53eda13c5eec4
Addis Belete <addis.bel@gmail.com>
2021-10-11 11:00:55 +0300

    Add a style to arrow class

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
-import { ArrowLeftOutlined } from '@material-ui/icons'
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
 const Container = styled.div`
 width: 100%;
 height:100vh;
 display: flex;
 `
-const Arrow = styled.div``
+const Arrow = styled.div`
+width: 50px;
+height:50px;
+background: white;
+border-radius: 50%;
+`
 const Slider = () => {
 	return (
 		<Container>
 			<Arrow>
 <ArrowLeftOutlined/>
+</Arrow>
+<Arrow>
+<ArrowRightOutlined>
 </Arrow>
 		</Container>
 	)
