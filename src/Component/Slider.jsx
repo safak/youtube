abeebec415441350a85dddc6ec3dd8a20a828f04
Addis Belete <addis.bel@gmail.com>
2021-10-11 10:56:29 +0300

    Import Arrow from material icons

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+import { ArrowLeftOutlined } from '@material-ui/icons'
 const Container = styled.div`
+width: 100%;
+height:100vh;
+display: flex;
 `
+const Arrow = styled.div``
 const Slider = () => {
 	return (
 		<Container>
-			
+			<Arrow>
+<ArrowLeftOutlined/>
+</Arrow>
 		</Container>
 	)
 }
