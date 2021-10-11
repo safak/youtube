af2f9a95451c23415f5f4695e73ff3b9994068c4
Addis Belete <addis.bel@gmail.com>
2021-10-11 11:41:43 +0300

    Add image component

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -1,41 +1,60 @@
-import React from 'react'
-import styled from 'styled-components'
-import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
+import React from "react";
+import styled from "styled-components";
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 const Container = styled.div`
-width: 100%;
-height:100vh;
-display: flex;
-background-color: coral;
-position: relative;
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+`;
 const Arrow = styled.div`
-width: 50px;
-height:50px;
-background: white;
-border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  background: white;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+`;
+const Slide = styled.div`
 display: flex;
 align-items: center;
-justify-content: center;
-position:absolute;
-top:0;
-bottom: 0;
-left:${props=>props.direction === "left" && "10px" };
-right:${props=> props.direction === "right" && "10px"};
-margin: auto;
-cursor: pointer;
-opacity:0.5;
+`;
+const ImgContainer = styled.div`
+flex:1;
 `
-const Slider = () => {
-	return (
-		<Container>
-			<Arrow direction = "left">
-<ArrowLeftOutlined/>
-</Arrow>
-<Arrow direction = "right">
-<ArrowRightOutlined/>
-</Arrow>
-		</Container>
-	)
-}
+const Image = styled.img`
+`
+const InfoContainer = styled.div``;
 
-export default Slider
+const Slider = () => {
+  return (
+    <Container>
+      <Arrow direction="left">
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper>
+<ImgContainer>
+<Image src = "https://i.ibb.co/XscdmR2c/1.png"/>
+</ImgContainer>
+<InfoContainer></InfoContainer>
+</Wrapper>
+      <Arrow direction="right">
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
