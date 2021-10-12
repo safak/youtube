7d308979b88afacd0068fc69ce4dfa2c4ed62047
Addis Belete <addis.bel@gmail.com>
2021-10-12 18:42:22 +0300

    Add CategoryItem Component

---
+++ src/Component/CategoryItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div``
+const CategoryItem = () => {
+	return (
+		<div>
+		<Container>
+</Container>	
+		</div>
+	)
+}
+
+export default CategoryItem
