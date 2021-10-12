afeecabe3cc1d9ce00145c6d540dfa9f34d21245
Addis Belete <addis.bel@gmail.com>
2021-10-12 16:38:03 +0300

    Add categories component

--- src/Data.js
+++ src/Data.js
@@ -1,34 +1,46 @@
 export const sliderItems = [
 	{
 		id: 1,
-		img: "https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png",
+		img:
+			"https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png",
 		title: "SUMMER SALE",
 		desc: "DONT'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
 		bg: "f5fafd",
-
-
 	},
 	{
 		id: 2,
-		img: "https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png",
+		img:
+			"https://images.pexels.com/photos/5600072/pexels-photo-5600072.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
 		title: "AUTUMN COLLECTION",
 		desc: "DONT'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
 		bg: "fcf1ed",
 	},
 	{
 		id: 3,
-		img: "https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png",
+		img:
+			"https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png",
 		title: "POPULAR SALE",
 		desc: "DONT'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
 		bg: "F6A840",
-	}
-]
+	},
+];
 export const categories = [
 	{
 		id: 1,
-		img: "https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+		img:
+			"https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
 		title: "SHIRT STYLE!",
-	}
-
-
-]
+	},
+	{
+		id: 2,
+		img:
+			"https://images.pexels.com/photos/792725/pexels-photo-792725.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+		title: "LOUNGEWEAR LOVE",
+	},
+	{
+		id: 3,
+		img:
+			"https://images.pexels.com/photos/6856000/pexels-photo-6856000.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+		title: "LIGHT JACKETS",
+	},
+];
