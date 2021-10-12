ef7c0692348505601ec1eb033dfd92a5c17df50d
Addis Belete <addis.bel@gmail.com>
2021-10-12 10:56:51 +0300

    Add Assets folder

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -26,17 +26,49 @@ const Arrow = styled.div`
 `;
 const Wrapper = styled.div`
   height: 100%;
+height: 100vh;
+display: flex;
 `;
 const Slide = styled.div`
+width: 100vw;
 display: flex;
 align-items: center;
 `;
 const ImgContainer = styled.div`
+height: 100%;
 flex:1;
+
 `
 const Image = styled.img`
+height: 80%;
+
+
 `
-const InfoContainer = styled.div``;
+const InfoContainer = styled.div`
+flex:1;
+padding: 50px;
+`;
+
+const Title = styled.h1`
+font-size: 70px;
+
+`
+const Desc = styled.p`
+margin: 50px 0px;
+font-size:20px;
+font-weight: 500;
+letter-spacing: 3px;
+
+
+`
+const Button = styled.button`
+padding: 10px;
+font-size: 20px;
+background-color: transparent;
+cursor: pointer;
+`	
+
+
 
 const Slider = () => {
   return (
@@ -45,10 +77,36 @@ const Slider = () => {
         <ArrowLeftOutlined />
       </Arrow>
       <Wrapper>
+<Slide>
 <ImgContainer>
-<Image src = "https://i.ibb.co/XscdmR2c/1.png"/>
+<Image src = "https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png"/>
 </ImgContainer>
-<InfoContainer></InfoContainer>
+<InfoContainer>
+<Title>SUMMER SALE</Title>
+<Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+<Button>SHOP NOW</Button>
+</InfoContainer>
+</Slide>
+ <Slide>
+<ImgContainer>
+<Image src = "https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png"/>
+</ImgContainer>
+<InfoContainer>
+<Title>SUMMER SALE</Title>
+<Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+<Button>SHOP NOW</Button>
+</InfoContainer>
+</Slide>
+<Slide>
+<ImgContainer>
+<Image src = "https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png"/>
+</ImgContainer>
+<InfoContainer>
+<Title>SUMMER SALE</Title>
+<Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+<Button>SHOP NOW</Button>
+</InfoContainer>
+</Slide>
 </Wrapper>
       <Arrow direction="right">
         <ArrowRightOutlined />
