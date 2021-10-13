0977854abd4cd328382204a78bdc717ce15def02
Addis Belete <addis.bel@gmail.com>
2021-10-13 11:01:01 +0300

    Import data to Products component

--- src/Component/Products.jsx
+++ src/Component/Products.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
+import {popularProducts} from '../Data'
+import Product from './Product'
 const Container = styled.div``
 const Products = () => {
 	return (
 		<Container>
-			
+{popularProducts.map((item)=>(
+<Product item={item}/>
+
+))}			
 		</Container>
 	)
 }
