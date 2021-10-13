d264a7d36d583a1d81e17319c9a3a15230447c24
Addis Belete <addis.bel@gmail.com>
2021-10-13 10:56:38 +0300

    Add Products and product component

---
+++ src/Component/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div``
+const Products = () => {
+	return (
+		<Container>
+			
+		</Container>
+	)
+}
+
+export default Products
