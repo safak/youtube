45bdf7931bc0b72b01d0ac57050ef975afc02083
Addis Belete <addis.bel@gmail.com>
2021-10-14 10:08:46 +0300

    Import socail Icons from material UI

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+`;
+
+
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Footer = () => {
-	return (
-		<div>
-			
-		</div>
-	)
-}
+  return (
+    <Container>
+      <Left>
+        <Logo>DONSA GEBEYA</Logo>
+        <Desc>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, nemo
+          vel. Impedit porro qui accusantium tempora quo labore, ducimus velit
+          soluta placeat ex voluptatibus, voluptates nisi numquam hic, officiis
+          rerum?
+        </Desc>
+        <SocialContainer>
+          <SocialIcon>
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon>
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon>
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon>
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center></Center>
+      <Right></Right>
+    </Container>
+  );
+};
 
-export default Footer
+export default Footer;
