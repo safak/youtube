c1a61f264fdd9c7ebb099850cae45198b4c4866d
Addis Belete <addis.bel@gmail.com>
2021-10-14 11:28:34 +0300

    Add a style to footer component

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,33 +1,78 @@
 import React from "react";
 import styled from "styled-components";
-import { Facebook, Twitter , Instagram, Pinterest} from "@material-ui/icons";
+import {
+  Facebook,
+  Twitter,
+  Instagram,
+  Pinterest,
+  Room,Phone,MailOutline
+} from "@material-ui/icons";
 const Container = styled.div`
   display: flex;
 `;
 const Left = styled.div`
   flex: 1;
-display: flex;
-flex-direction: column ;
-padding: 20px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
 `;
-const Logo = styled.h1``
+const Logo = styled.h1``;
 
-const Desc = styled.p``
+const Desc = styled.p`
+  margin: 20px 0;
+`;
 
+const SocialContainer = styled.div`
+  display: flex;
+`;
 
-const SocialContainer = styled.div``
-
-const SocialIcon = styled.div``
-
-
-
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
 `;
-const Right = styled.div`
-  flex: 1;
+const Title = styled.h3`
+  margin-bottom: 30px;
 `;
 
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+margin-bottom: 20px;
+display: flex;
+align-items: center;
+
+
+`;
+const Payment = styled.img`
+width: 50%;
+`;
 const Footer = () => {
   return (
     <Container>
@@ -40,22 +85,43 @@ const Footer = () => {
           rerum?
         </Desc>
         <SocialContainer>
-          <SocialIcon>
+          <SocialIcon color="3B5999">
             <Facebook />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E4405F">
             <Instagram />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="55ACEE">
             <Twitter />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E60023">
             <Pinterest />
           </SocialIcon>
         </SocialContainer>
       </Left>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style = {{marginRight:'10px'}}/> Ethio-china Street, Addis Ababa
+        </ContactItem>
+        <ContactItem><Phone style = {{marginRight:'10px'}}/>+251943413849</ContactItem>
+        <ContactItem><MailOutline style = {{marginRight:'10px'}}/>Addis.bel@gmail.com</ContactItem>
+<Payment src="https://i.ibb.co/Qfvn4z6/payment.png"/>
+      </Right>
     </Container>
   );
 };
