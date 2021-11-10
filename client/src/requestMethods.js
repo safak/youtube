d92655d769dde5ad9035797251ca28339318c6a2
Safak <safakkocaoglu@hotmail.com>
2021-11-10 19:54:49 +0100

    fixed: TOKEN issue

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-    .accessToken || "";
+// const TOKEN =
+//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
+//     .accessToken || "";
+
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
