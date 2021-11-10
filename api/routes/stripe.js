3e75b80b37fa2e7bc5eedd94de36bdaeae74492a
Safak <safakkocaoglu@hotmail.com>
2021-11-10 20:38:40 +0100

    fixed: Stripe Key issue

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_KEY
+const stripe = require("stripe")(KEY);
 
 router.post("/payment", (req, res) => {
   stripe.charges.create(
