7e6348d9d9597fce4d0046184e02a7adfdf89b74
Aniket524 <81089019+Aniket524@users.noreply.github.com>
2022-01-17 12:39:05 +0530

    Revert "sorry-bymistake"
    This reverts commit 7cd2e914e74a13d296eeeafcd989ce75d19bf1b7.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import About from './components/pages/about';
-import Shop from './components/pages/shop';
 
 ReactDOM.render(
-  <BrowserRouter>
   <React.StrictMode>
-    <Routes>
-      <Route path="/" element={<App/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/shop" element={<Shop/>}/>
-    </Routes>
-  </React.StrictMode>
-  </BrowserRouter>,
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
