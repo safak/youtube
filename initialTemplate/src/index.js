1bc96a1040fd2bedef2525e33f51b791dc327807
Hüseyin Türkmenoğlu <turkmenogluhuseyin@gmail.com>
2022-06-08 13:53:12 +0300

    Remove store and Provide for initialTemplate
    Initial Template shouldn't have a  store and Provider.

--- initialTemplate/src/index.js
+++ initialTemplate/src/index.js
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import store from "./redux/store";
-import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <Provider store={store}>
-      <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
