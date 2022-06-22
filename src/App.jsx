26d51e83099c080b73431cf13e3ee0d307d7288c
Prakash-Reddy08 <mjprakashreddy1@gmail.com>
2022-06-22 13:27:41 +0530

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,39 @@
-import { useState } from "react";
-import "./app.css";
-import FormInput from "./components/FormInput";
+import { useState } from 'react';
+import './app.css';
+import FormInput from './components/FormInput';
+import inputs from './utils/inputs';
 
-const App = () => {
-  const [values, setValues] = useState({
+function App() {
+
+  const [formData, setFormData] = useState({
     username: "",
     email: "",
     birthday: "",
     password: "",
-    confirmPassword: "",
-  });
-
-  const inputs = [
-    {
-      id: 1,
-      name: "username",
-      type: "text",
-      placeholder: "Username",
-      errorMessage:
-        "Username should be 3-16 characters and shouldn't include any special character!",
-      label: "Username",
-      pattern: "^[A-Za-z0-9]{3,16}$",
-      required: true,
-    },
-    {
-      id: 2,
-      name: "email",
-      type: "email",
-      placeholder: "Email",
-      errorMessage: "It should be a valid email address!",
-      label: "Email",
-      required: true,
-    },
-    {
-      id: 3,
-      name: "birthday",
-      type: "date",
-      placeholder: "Birthday",
-      label: "Birthday",
-    },
-    {
-      id: 4,
-      name: "password",
-      type: "password",
-      placeholder: "Password",
-      errorMessage:
-        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
-      label: "Password",
-      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
-      required: true,
-    },
-    {
-      id: 5,
-      name: "confirmPassword",
-      type: "password",
-      placeholder: "Confirm Password",
-      errorMessage: "Passwords don't match!",
-      label: "Confirm Password",
-      pattern: values.password,
-      required: true,
-    },
-  ];
-
+    confirmPassword: ""
+  })
   const handleSubmit = (e) => {
     e.preventDefault();
-  };
+    console.log(e)
+  }
 
-  const onChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
 
   return (
     <div className="app">
-      <form onSubmit={handleSubmit}>
+      <section>
         <h1>Register</h1>
-        {inputs.map((input) => (
-          <FormInput
-            key={input.id}
-            {...input}
-            value={values[input.name]}
-            onChange={onChange}
-          />
-        ))}
-        <button>Submit</button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          {inputs(formData).map((input) => {
+            return <FormInput handleChange={handleChange} key={input.id} {...input} value={formData[input.name]} />
+          })}
+          <button>Submit</button>
+        </form>
+      </section>
     </div>
   );
-};
+}
 
 export default App;
