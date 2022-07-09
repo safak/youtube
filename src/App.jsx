3df2230aa17dc8a94423ca50dea41311bd591c09
AnsonAU10 <anson456654@gmail.com>
2022-07-10 03:28:04 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Home from "./pages/Home";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return <Home />;
 };
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Homepage</div>;
+};
+
+export default Home;
