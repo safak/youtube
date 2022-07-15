a0aa3dac68e3ae5231addef33f621411c15538fe
darkpsinight <60811247+darkpsinight@users.noreply.github.com>
2022-07-15 18:46:25 +0100

    update cartRedux.js code (fix issue)
    I tested your code by adding one product with a price 30$.
    And then i added to cart two of the same previous product (30$ * 2 = 60 $).
    The total should give 90$ (30$+60$).
    But with your code, the result is 150$.
    Now with this fix, it gives 90$.

--- client/src/redux/cartRedux.js
+++ client/src/redux/cartRedux.js
@@ -11,7 +11,7 @@ const cartSlice = createSlice({
     addProduct: (state, action) => {
       state.quantity += 1;
       state.products.push(action.payload);
-      state.total += action.payload.price * action.payload.quantity;
+      state.total += action.payload.price;
     },
   },
 });
