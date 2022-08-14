d4dfccfe9289c4829c67f396aa4f244b83636419
Akshat Mehta <akshat2404@gmail.com>
2022-08-14 16:41:34 +0530

    Footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Announcement from '../components/Announcement'
 import Categories from '../components/Categories'
+import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
 import Products from '../components/Products'
@@ -15,6 +16,7 @@ const Home = () => {
       <Categories/>
       <Products/>
       <Newsletter/>
+      <Footer/>
     </div>
   )
 }
