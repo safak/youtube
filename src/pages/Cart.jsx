bb6068259f8f26f976616a3f037fb237efcdfe4f
Suraj Subedi <41540064+Suraj-Subedi@users.noreply.github.com>
2022-10-10 14:30:18 +0545

    Update cart.jsx
    Just simple swap of components.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -156,8 +156,8 @@ const Button = styled.button`
 const Cart = () => {
   return (
     <Container>
+     <Announcement />
       <Navbar />
-      <Announcement />
       <Wrapper>
         <Title>YOUR BAG</Title>
         <Top>
