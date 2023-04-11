add35ccfc42203b6ac100d59048150e02b7d0cc1
saaidin <dinme73@gmail.com>
2023-04-11 17:25:49 +0800

    Add Admin & Password

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -1,34 +1,34 @@
-import dbConnect from "../../../util/mongo";
-import Order from "../../../models/Order";
+import dbConnect from "../../../util/mongo"
+import Order from "../../../models/Order"
 
 const handler = async (req, res) => {
   const {
     method,
     query: { id },
-  } = req;
+  } = req
 
-  await dbConnect();
+  await dbConnect()
 
   if (method === "GET") {
     try {
-      const order = await Order.findById(id);
-      res.status(200).json(order);
+      const order = await Order.findById(id)
+      res.status(200).json(order)
     } catch (err) {
-      res.status(500).json(err);
+      res.status(500).json(err)
     }
   }
   if (method === "PUT") {
     try {
       const order = await Order.findByIdAndUpdate(id, req.body, {
         new: true,
-      });
-      res.status(200).json(order);
+      })
+      res.status(200).json(order)
     } catch (err) {
-      res.status(500).json(err);
+      res.status(500).json(err)
     }
   }
   if (method === "DELETE") {
   }
-};
+}
 
-export default handler;
+export default handler
