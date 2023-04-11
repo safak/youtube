adf5a3f18857e4f7383d8a401f0bb48699b0d5ab
saaidin <dinme73@gmail.com>
2023-04-11 17:07:08 +0800

    admin dashboard page

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -4,7 +4,7 @@ import Product from "../../../models/Product"
 export default async function handler(req, res) {
   const { method, cookies } = req
 
-  // const token = cookies.token
+  const token = cookies.token
 
   dbConnect()
 
@@ -18,9 +18,9 @@ export default async function handler(req, res) {
   }
 
   if (method === "POST") {
-    // if (!token || token !== process.env.token) {
-    //   return res.status(401).json("Not authenticated!")
-    // }
+    if (!token || token !== process.env.token) {
+      return res.status(401).json("Not authenticated!")
+    }
     try {
       const product = await Product.create(req.body)
       res.status(201).json(product)
